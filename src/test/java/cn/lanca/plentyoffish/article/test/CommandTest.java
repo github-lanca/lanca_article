17d5700be54557ed7d45e5e3aeb285f6f3826917
hongliang.mei <casblanca@163.com>
2019-12-11 10:46:22 +0800

    update

---
+++ src/test/java/cn/lanca/plentyoffish/article/test/CommandTest.java
@@ -0,0 +1,19 @@
+package cn.lanca.plentyoffish.article.test;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CommandTest {
+    //Mac系统快捷键记录
+    //1.command + d(duplicate)     复制一行代码
+    //2.command + 退格/回退/⬅       删除光标所在行
+    //3.command + option(alt) + L️  格式化代码
+    //4.command + /                注释/取消注释
+    //5.command + shift + u        选择内容大小写切换
+    //6.shift + enter              快速切换到下一行
+    //7.command + option(alt)  左右箭头    回退/前进到之前操作位置
+
+}

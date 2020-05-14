0050807ea18f6b205ed208ac18b84afe12051044
mhl <cncommdata.mei>
2020-05-14 10:52:13 +0800

    提交

---
+++ src/test/java/cn/lanca/plentyoffish/article/service/impl/ArticleServiceImplTest.java
@@ -0,0 +1,20 @@
+package cn.lanca.plentyoffish.article.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author meihongliang
+ * @since 2020/5/14 09:54
+ */
+class ArticleServiceImplTest {
+
+    @Test
+    void addArticleInfo() {
+    }
+
+    @Test
+    void getAllArticleListInfo() {
+    }
+}
--- src/test/java/cn/lanca/plentyoffish/article/test/CommandTest.java
+++ src/test/java/cn/lanca/plentyoffish/article/test/CommandTest.java
@@ -1,5 +1,6 @@
 package cn.lanca.plentyoffish.article.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -15,5 +16,15 @@ public class CommandTest {
     //5.command + shift + u        选择内容大小写切换
     //6.shift + enter              快速切换到下一行
     //7.command + option(alt)  左右箭头    回退/前进到之前操作位置
+
+    @Test
+    public void test(){
+
+        // 关于电梯载客效率的猜想
+        // #1.早高峰,所有电梯从1楼集中分散至其他楼层,如何设置或调整电梯运行模式,使乘客最大效率的上升至其目标楼层
+        // #2.电梯需求淡季,任何楼层响应,至任何楼层
+        // #3.晚高峰,所有楼层都可能有乘客需求下至1楼.
+        // #4.模拟大厦层高50,三部电梯,如何设计
+    }
 
 }
